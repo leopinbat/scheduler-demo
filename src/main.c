ad6b1cadfbe56e5f6a1e0da810f277c491d00af2
leopinbat <leopinbat>
2017-10-01 18:29:26 -0300

    Final_Program

--- src/main.c
+++ src/main.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
 
 int main() {
-  int a[5];
-  int n;
+  int var0, var1, var2, var3, var4, n;
+  scanf("%d %d %d %d %d %d", &var0, &var1, &var2, &var3, &var4, &n);
 
-  scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
-
-  for (int i=0; i<n; i++) {
+  for (int i=1; i<=n; i++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
+	
+	
+ 	if((i%var0!= 0)&&(i%var1!= 0)&&(i%var2!= 0)&&(i%var3!= 0)&&(i%var4!= 0)){
+		printf ("-");
+	}
+	
+	else {
+		if(i%var0 == 0)
+			printf("%d", 0);
+
+		if(i%var1 == 0)
+			printf("%d", 1);
+
+		if(i%var2 == 0)
+			printf("%d", 2);
+
+		if(i%var3 == 0)
+			printf("%d", 3);
+
+		if(i%var4 == 0)
+			printf("%d", 4);
+	}
+	
+	printf ("\n");
+	
   }
 
-  return 0;
+  return(0);
 }
